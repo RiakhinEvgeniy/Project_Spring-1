d42afaedc22bef1ff9b727d91eba78d6d340c1f5
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-07-29 18:35:09 +0200

    Added class entity

---
+++ src/main/java/com/evgeniy/riakhin/domain/Task.java
@@ -0,0 +1,23 @@
+package com.evgeniy.riakhin.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(schema = "todo", name = "task")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Enumerated(EnumType.ORDINAL)
+    private Status status;
+}
