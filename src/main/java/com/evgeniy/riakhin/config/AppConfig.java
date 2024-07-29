a7c8da86adcde0ec4ed36aacbcb073262ce51f5e
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-07-29 19:41:59 +0200

    Added to GitHub

---
+++ src/main/java/com/evgeniy/riakhin/config/AppConfig.java
@@ -0,0 +1,54 @@
+package com.evgeniy.riakhin.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.cfg.Environment;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+
+@Configuration
+@EnableTransactionManagement
+public class AppConfig {
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(getDataSource());
+        sessionFactory.setPackagesToScan("com.evgeniy.riakhin");
+        sessionFactory.setHibernateProperties(getHibernateProperties());
+        return sessionFactory;
+    }
+
+    private Properties getHibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+        hibernateProperties.put(Environment.HBM2DDL_AUTO, "validate");
+        return hibernateProperties;
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        try (HikariDataSource dataSource = new HikariDataSource()) {
+            dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/todo");
+            dataSource.setUsername("root");
+            dataSource.setPassword("1234");
+            return dataSource;
+        }
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory factory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(factory);
+        return transactionManager;
+    }
+}
---
+++ src/main/java/com/evgeniy/riakhin/config/WebConfig.java
@@ -0,0 +1,50 @@
+package com.evgeniy.riakhin.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.evgeniy.riakhin")
+@AllArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final ApplicationContext applicationContext;
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext(applicationContext);
+        resolver.setPrefix("/html/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setCacheable(false);
+        return resolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(templateResolver());
+        return engine;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("tasks");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    }
+}
